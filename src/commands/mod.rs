e98dfcf09435d00c09485252d42f22fe120b13ae
defnot001 <marc.miller994@gmail.com>
2023-05-07 20:27:06 +0200

    changed commands macro

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,16 +1,21 @@
 use serenity::builder::CreateApplicationCommands;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::client::Context;
+use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+
+use crate::error::Error;
 
 macro_rules! commands {
     ($(mod $name:ident;)*) => {
         $(pub mod $name;)*
 
-        pub fn run_command(name: &str, options: &[CommandDataOption]) -> String {
+        pub async fn run_command(interaction: &ApplicationCommandInteraction, ctx: &Context) -> Result<(), Error> {
+          let name = interaction.data.name.as_str();
+
           match name {
             $(
-              stringify!($name) => $name::run(options),
+              stringify!($name) => $name::execute(interaction, &interaction.data.options, ctx).await,
             )*
-            _ => "not implemented".to_string(),
+            _ => Err(Error::Serenity(serenity::Error::Other("Unknown Interaction."))),
           }
         }
 
@@ -21,7 +26,5 @@ macro_rules! commands {
 }
 
 commands! {
-  mod attachmentinput;
-  mod id;
-  mod ping;
+  mod animal;
 }
