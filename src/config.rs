b7ccc42ea1fc9aa046d9db83f6176ca663d6a6c0
defnot001 <marc.miller994@gmail.com>
2024-04-05 10:32:58 +0200

    derive Clone for Config

--- src/config.rs
+++ src/config.rs
@@ -3,20 +3,20 @@ use std::num::NonZeroU64;
 use anyhow::Context;
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Config {
     pub bot: BotConfig,
     pub database: DatabaseConfig,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct BotConfig {
     pub token: String,
     pub client_id: NonZeroU64,
     pub guild_id: NonZeroU64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct DatabaseConfig {
     pub url: String,
 }
