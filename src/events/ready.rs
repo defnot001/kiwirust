0a4581a387bf1a7c4a426cfaf4f977a10c7f21e4
defnot001 <marc.miller994@gmail.com>
2024-04-05 10:32:13 +0200

    fix: remove "to" already included in activity type

--- src/events/ready.rs
+++ src/events/ready.rs
@@ -7,7 +7,7 @@ pub async fn handle_ready(
     tracing::info!("Logged in as {}.", data_about_bot.user.name);
 
     ctx.set_activity(Some(serenity::ActivityData {
-        name: "to commands".to_string(),
+        name: "commands".to_string(),
         kind: serenity::ActivityType::Listening,
         state: None,
         url: None,
