a9de4bc87523733f5dae93e2db8d4eb8549ca8a4
defnot001 <marc.miller994@gmail.com>
2024-04-06 22:20:44 +0200

    finish info command

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -1,12 +1,12 @@
 use anyhow::Context;
 use poise::CreateReply;
-use serenity::all::{PartialGuild, RichInvite, RoleId, User};
+use serenity::all::{Member, PartialGuild, RichInvite, RoleId, User};
 use serenity::builder::{CreateAttachment, CreateInvite};
 use url::Url;
 
 use crate::error::respond_error;
 use crate::util::builder::default_embed;
-use crate::util::format::{display_time};
+use crate::util::format::display_time;
 use crate::Context as AppContext;
 
 /// Get information.
@@ -58,6 +58,44 @@ async fn user(
     ctx: AppContext<'_>,
     #[description = "Select a user."] target: User,
 ) -> anyhow::Result<()> {
+    let Some(guild) = ctx.partial_guild().await else {
+        anyhow::bail!("Cannot find the guild this interaction was created in")
+    };
+
+    let name = target
+        .global_name
+        .as_deref()
+        .unwrap_or(target.name.as_str());
+    let avatar_url = target.avatar_url().unwrap_or(target.default_avatar_url());
+    let joined_discord = display_time(target.created_at().to_utc());
+
+    let embed = default_embed(ctx.author())
+        .title(format!("User Info {name}"))
+        .thumbnail(avatar_url)
+        .field("Username", target.name.as_str(), false)
+        .field("User ID", target.id.to_string(), false)
+        .field("Joined Discord", joined_discord, false);
+
+    let embed = if let Ok(member) = guild.member(&ctx, &target.id).await {
+        let embed = if let Some(joined) = member.joined_at {
+            embed.field(
+                format!("Joined {}", guild.name),
+                display_time(joined.to_utc()),
+                false,
+            )
+        } else {
+            embed
+        };
+
+        let roles = display_member_roles(&member, &guild, &ctx).await;
+
+        embed.field("Roles", roles, false)
+    } else {
+        embed
+    };
+
+    ctx.send(CreateReply::default().embed(embed)).await?;
+
     Ok(())
 }
 
@@ -110,10 +148,18 @@ async fn avatar(
     ctx: AppContext<'_>,
     #[description = "Select a user."] target: User,
 ) -> anyhow::Result<()> {
-    let mut avatar_url = Url::parse(target.avatar_url().unwrap_or(target.default_avatar_url()).as_str())?;
+    let mut avatar_url = Url::parse(
+        target
+            .avatar_url()
+            .unwrap_or(target.default_avatar_url())
+            .as_str(),
+    )?;
 
     if avatar_url.query().is_none() {
-        anyhow::bail!("Failed to parse url size query param in url: {}", avatar_url.as_str());
+        anyhow::bail!(
+            "Failed to parse url size query param in url: {}",
+            avatar_url.as_str()
+        );
     };
 
     avatar_url.set_query(Some("size=4096"));
@@ -123,7 +169,6 @@ async fn avatar(
     ctx.send(CreateReply::default().attachment(attachment))
         .await
         .context("Failed to send message")?;
-
 
     Ok(())
 }
@@ -145,13 +190,15 @@ async fn create_invite(ctx: &AppContext<'_>) -> anyhow::Result<RichInvite> {
             )
             .await
             .context("Failed to create invite"),
-        None => {
-            Err(anyhow::anyhow!("Failed to get invite channel channel."))
-        }
+        None => Err(anyhow::anyhow!("Failed to get invite channel channel.")),
     }
 }
 
-async fn get_member_names_per_role(ctx: &AppContext<'_>, guild: &PartialGuild, role_id: &RoleId) -> anyhow::Result<Vec<String>> {
+async fn get_member_names_per_role(
+    ctx: &AppContext<'_>,
+    guild: &PartialGuild,
+    role_id: &RoleId,
+) -> anyhow::Result<Vec<String>> {
     let members = guild.members(&ctx, None, None).await?;
 
     let mut member_names = Vec::new();
@@ -165,4 +212,33 @@ async fn get_member_names_per_role(ctx: &AppContext<'_>, guild: &PartialGuild, r
     member_names.sort_unstable();
 
     Ok(member_names)
+}
+
+async fn display_member_roles(
+    member: &Member,
+    guild: &PartialGuild,
+    ctx: &AppContext<'_>,
+) -> String {
+    let Some(roles) = member.roles(ctx) else {
+        return "None".to_string();
+    };
+
+    if roles.is_empty() {
+        return "None".to_string();
+    }
+
+    // filter out the @everyone role
+    let mut roles = roles
+        .into_iter()
+        .filter(|role| role.id != RoleId::from(guild.id.get()))
+        .collect::<Vec<_>>();
+
+    roles.sort_unstable();
+    roles.reverse();
+
+    roles
+        .iter()
+        .map(|role| format!("<@&{}>", role.id))
+        .collect::<Vec<_>>()
+        .join(", ")
 }
