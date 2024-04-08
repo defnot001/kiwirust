dc2c34c8b05f3d5602f5c533a2b8cc54d64293a0
defnot001 <marc.miller994@gmail.com>
2024-04-08 14:55:42 +0200

    add fn to get server state from pterodactyl

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,3 +2,4 @@ pub mod builder;
 pub mod format;
 pub mod mojang;
 pub mod rcon;
+pub mod pterodactyl;
---
+++ src/util/pterodactyl.rs
@@ -0,0 +1,35 @@
+use std::fmt::Display;
+
+use pterodactyl_api::client::{ClientBuilder, ServerState};
+
+use crate::config::{PterodactylConfig, ServerConfig};
+
+pub struct PteroClient;
+pub struct DisplayServerState(pub ServerState);
+
+impl PteroClient {
+    pub async fn server_state(
+        ptero_config: &PterodactylConfig,
+        server_config: &ServerConfig,
+    ) -> anyhow::Result<ServerState> {
+        Ok(
+            ClientBuilder::new(ptero_config.url.clone(), ptero_config.api_key.clone())
+                .build()
+                .get_server(server_config.panel_id.clone())
+                .get_resources()
+                .await?
+                .current_state,
+        )
+    }
+}
+
+impl Display for DisplayServerState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            ServerState::Offline => write!(f, "offline"),
+            ServerState::Running => write!(f, "running"),
+            ServerState::Starting => write!(f, "starting"),
+            ServerState::Stopping => write!(f, "stopping")
+        }
+    }
+}
